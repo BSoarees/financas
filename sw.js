e4a8519b30b8a230ff611f1c0de1e26c57c35fa6
Bernardo Soares <bernardopsoares20@gmail.com>
2026-09-20 15:37:56 -0300

    Separate investments from reserve category

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* cache so do casco do app. dado nunca passa por aqui. */
-const CACHE = "financas-v4";
+const CACHE = "financas-v5";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
